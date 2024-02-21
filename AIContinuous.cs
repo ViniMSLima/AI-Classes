873c9fa881cf2464abf399dca2ee967d9236590d
ViniMSLima <vinisary.lima@gmail.com>
2024-02-21 14:50:32 -0300

    Newton's Method

--- AIContinuous.cs
+++ AIContinuous.cs
@@ -5,7 +5,7 @@ namespace AIContinuous
     public static class Root
     {
         public static double Bisection(Func<double, double> function, double a, double b, double atol = 1e-4,
-                                        double rtol = 1e-4, int maxIter = 1000)
+                                        double rtol = 1e-6, int maxIter = 1000)
         {
             double x0;
             double x2;
@@ -47,43 +47,55 @@ namespace AIContinuous
         public static double FalsePosition(Func<double, double> function, double a, double b, double atol = 1e-4,
                                             double rtol = 1e-4, int maxIter = 1000)
         {
-            if (a < 0 || b < 0)
-            {
-                WriteLine("Math.sqrt() can't resolve negative number");
-                return 0;
-            }
-
-            double x0, y0, y1, x1;
-            double x2 = 0;
+            double y0, y1;
+            double c = 0;
 
             for (int i = 0; i < maxIter; i++)
             {
                 y0 = function(a);
                 y1 = function(b);
 
-                x2 = (-y0) * ((b - a) / (y1 - y0)) + a;
+                c = (-y0) * ((b - a) / (y1 - y0)) + a;
+                var fc = function(c);
 
-                if (x2 * y0 > 0)
-                    a = x2;
+                if (fc * y0 < 0.0)
+                    b = c;
                 else
-                    b = x2;
-
-                if (b - a < rtol * 2)
-                {
-                    WriteLine("B - A");
-                    break;
-                }
-
-                var calcTol = function(x2);
+                    a = c;
+                
+                var calcTol = fc;
                 if (Math.Abs(calcTol) < atol)
                 {
                     WriteLine("ABSOLUTE");
                     break;
                 }
+
+                if (b - a < 2.0 * rtol)
+                {
+                    WriteLine("B - A");
+                    break;
+                }
             }
 
-            return x2;
+            return c;
+        }
+
+        public static double Newton(Func<double, double> function, Func<double, double> der, double x0, 
+                                        double atol = 1e-6, int maxIter = 10000)
+        {
+            double xp = x0;
+
+            for (int i = 0; i < maxIter; i++)
+            {
+                var fp = function(xp);
+
+                xp -= fp / der(xp);
+
+                if(Math.Abs(fp) < atol)
+                    break;
+            }
+
+            return xp;
         }
     }
 }
- 
--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 double MyFunction(double x)
 {
-    return Math.Sqrt(x) - Math.Cos(x);
+    return (Math.Sqrt(Math.Abs(x)) - 5) * x + 10;
 }
 
+double MyDer(double x)
+{
+    return (x / (2* Math.Sqrt(Math.Abs(x)))) + (Math.Sqrt(Math.Abs(x)) - 5);
+}
+
+double sol;
 var data = DateTime.Now;
-var sol = Root.FalsePosition(MyFunction, 0, 1);
+data = DateTime.Now;
+
+// sol = Root.Bisection(MyFunction, -10, 10);
+// sol = Root.FalsePosition(MyFunction, -10, 10);
+sol = Root.Newton(MyFunction, MyDer, 10);
+
 
 Console.WriteLine("\nExecution time: " + (DateTime.Now - data).TotalMilliseconds + " ms");
 Console.WriteLine("Resultado: " + sol);
